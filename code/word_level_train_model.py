981cfbd287ad097a49426c0dae6d452c067ecdf3
EC2 Default User <ec2-user@ip-172-31-9-162.us-west-2.compute.internal>
2017-10-18 15:14:22 +0000

    word model result

--- code/word_level_train_model.py
+++ code/word_level_train_model.py
@@ -7,7 +7,7 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 # load text and covert to lowercase
-filename = '/Workspace-Github/text_generator_keras/data/wonderland.txt'
+filename = '/home/ec2-user/text_generator_keras/data/wonderland.txt'
 raw_text = open(filename).read().lower()
 # create mapping of unique chars to integers
 tokens = sorted(list(set(nltk.word_tokenize(raw_text))))
@@ -44,7 +44,7 @@ model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 # define the checkpoint
-filepath = '/Workspace-Github/text_generator_keras/data/weight_word/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5'
+filepath = 'weights-improvement-{epoch:02d}-{loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 # fit the model
